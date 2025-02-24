1ec220868477edb42a9c78944fcb36a530250b04
Dhruv Suthar <dhruvgajjar512@gmail.com>
2025-02-24 14:38:17 +0530

    first commit

---
+++ all_queries.js
@@ -0,0 +1,249 @@
+// 1.How many users are active?
+[
+  {
+    $match: {
+      isActive: true,
+    },
+  },
+  {
+    $count: "activeUsers",
+  },
+];
+
+// 2.what is the average age of all users?
+
+[
+  {
+    $group: {
+      _id: null,
+      averageAge:
+      {
+        $avg: "$age"
+      }
+      
+      }
+  }
+  
+]
+
+//3.List the top 5 most common favorite fruits grouped among the users 
+
+//->acending order
+[ 
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      count:{
+        $sum:1
+      }
+      
+    },
+  },
+  {
+    $sort: {
+      count: 1
+    }
+  }
+]
+
+//->decending order
+[ 
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      count:{
+        $sum:1
+      }
+      
+    },
+  },
+  {
+    $sort: {
+      count: -1
+    }
+  }
+]
+
+//->limit up 2 wants to see top 2
+[ 
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      count:{
+        $sum:1
+      }
+      
+    },
+  },
+  {
+    $sort: {
+      count: -1
+    }
+  },
+  {
+    $limit: 2
+  }
+]
+
+//4 Find the total number of male and females?
+
+[
+  {
+    $group: {
+      _id: "$gender",
+      genderCount:{
+        $sum:1
+      }
+     
+    }
+  }
+ ]
+
+ //5 Which country has the highest total number of registerd users?
+ [
+  {
+    $group: {
+      _id:"$company.location.country",
+      userCount:{
+        $sum:1 //each and every country get the sum of total number
+      }
+     
+    }
+  },
+   {
+     $sort: {
+       userCount: -1 //decending order provide the code
+     }
+   },
+   {
+    $limit:3 //top 3 result gives
+  }
+ ]
+
+ //6.List all unique eye colors present in the collection.
+ [
+  {
+    $group: {
+      _id:"$eyeColor",
+      
+    }
+  }
+ ]
+
+ //7..what is the average number of tags per user?
+
+ //1-> First approch
+ [
+  {
+    $unwind: "$tags" //This will use separate documents parse and construct the array.
+     
+  },
+  {
+  	$group: {
+  	  _id:"$_id",
+      numberofTags:{ //This the use total sum of tags 
+        $sum:1
+      }
+  	}  
+  },
+  {
+    $group: {
+      _id: null,
+      averageNumberofTags:{$avg:"$numberofTags"} //This tags numberof tags sum and get a avg find.
+      
+    }
+  }
+]
+
+ //2-> second approch
+
+ [
+  {
+    $addFields: {
+  			numberofTags:{
+          $size:{$ifNull:["$tags",[]]} //If conditon apply because of array is null then empty array define
+        }
+    }
+  },
+  {
+    $group: {
+      _id: null,
+     averageNumberofTags:{$avg:"$numberofTags"} //This array apply get a averageof number tags will get the result
+    }
+  }
+]
+
+
+//8.How many users have 'enim' as one of their tags?
+[
+  {
+    $match: {
+    		tags:"enim"
+    }
+  },{
+    $count: 'userenimTag'
+  }
+]
+
+
+//9.what are the names and age of users who are inactive and have 'velit' as a tag?
+
+[
+  {
+    $match: {
+    	isActive:false,
+      tags:"velit"
+    }
+  },{
+    $project: {
+      name:1,age:1
+    }
+  }
+]
+
+//10.How many users have a phone number starting with '+1 (940);?
+
+[
+  {
+    $match: {
+      "company.phone": /^\+1 \(940\)/
+    }
+  },
+  {
+    $count: "userswithspecialphone",
+  }
+]
+
+// 11.who has registerd the most recently?
+[
+  {
+    $sort: {
+      registered: -1
+    }
+  },
+  {
+    $limit: 4
+  },
+  {
+    $project: {
+      name:1,
+      registered:1,
+      favoriteFruit:1
+    }
+  }
+]
+
+// 12.categorize users by their favorite fruits?
+
+[
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      users:{$push:"$name"},
+      
+      
+    }
+  }
+]
+
+
